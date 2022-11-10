b45fe3105c8ea3095a5ed10c4e1a4355eda8fa23
Geo <mrgeooo14@gmail.com>
2022-11-10 16:11:11 +0100

    project1 - get data

--- project1/generate.py
+++ project1/generate.py
@@ -1,4 +1,20 @@
 import torch
-import  dlc_practical_prologue
+import  dlc_practical_prologue as prologue
 
-print('libraries imported')
+def get_sets(nb):
+    sets = prologue.generate_pair_sets(nb)
+    
+    train_set = sets[0]
+    train_target = sets[1]
+    train_classes = sets[2]
+
+    test_set = sets[3]
+    test_target = sets[4]
+    test_classes = sets[5]
+
+    return train_set, train_target, train_classes, test_set, test_target, test_classes
+
+
+if __name__ == "__main__":
+    sample_size = 1000
+    train_set, train_target, train_classes, test_set, test_target, test_classes = get_sets(sample_size)
